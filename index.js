75b319f25071b05b64ac84c97d435b97bf25b9eb
lqkii <chowl801@newschool.edu>
2021-10-19 16:05:10 -0400

    mac

---
+++ index.js
@@ -0,0 +1,142 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".l1", {
+    scrollTrigger: {
+        trigger: ".l1"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l2", {
+    scrollTrigger: {
+        trigger: ".l2"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l3", {
+    scrollTrigger: {
+        trigger: ".l3"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l4", {
+    scrollTrigger: {
+        trigger: ".l4"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l5", {
+    scrollTrigger: {
+        trigger: ".l5"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l6", {
+    scrollTrigger: {
+        trigger: ".l6"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l7", {
+    scrollTrigger: {
+        trigger: ".l7"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l8", {
+    scrollTrigger: {
+        trigger: ".l8"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l9", {
+    scrollTrigger: {
+        trigger: ".l9"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l10", {
+    scrollTrigger: {
+        trigger: ".l10"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l11", {
+    scrollTrigger: {
+        trigger: ".l11"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l12", {
+    scrollTrigger: {
+        trigger: ".l12"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l13", {
+    scrollTrigger: {
+        trigger: ".l13"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
+gsap.to(".l14", {
+    scrollTrigger: {
+        trigger: ".l14"
+    },
+    y: -200,
+    duration: 1.4,
+    immediateRender: false,
+    ease: "expo.OutOut"
+});
+
